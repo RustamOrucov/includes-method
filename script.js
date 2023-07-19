7748f45f0ec4856d5a7896c02938203f9e6bcc53
orucov <orucov.r@inbox.ru>
2023-07-19 12:05:11 +0400

    forEach

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-const arr = ['1','2','3','4'];
-
-function inArr() {
-  let num = prompt("bir eded daxil edin");
-  if (arr.includes(num) === true) {
-    return true;
-  } else {
-    return false;
-  }
+const arr = ['1','2','3','4','5'];
+const arr2=['3','4','5','6','7']
+function conArr() {
+     let newArr=[]
+  arr.forEach(el => {
+       arr2.forEach(el1=>{
+          if(el===el1){
+            newArr.push(el)
+          }
+       })
+  });
+return newArr
 }
-console.log(inArr());
+console.log(conArr());
